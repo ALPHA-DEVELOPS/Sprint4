e1d6353854005f2cef1bdee2184be819967d900b
Camilo Mora <cm.prog0@gmail.com>
2022-11-08 21:17:10 -0500

    backend base

---
+++ Back/server.js
@@ -0,0 +1,14 @@
+import Express from "express";
+import cors from 'cors' 
+const app = Express()
+
+app.use(cors())
+app.listen('5000', function(){
+    console.log("Servidor iniciado.")
+})
+
+
+app.get('/consultar', function(pet, res){
+    console.log("Conectado al servidor")
+    res.send("Conectado")
+})
